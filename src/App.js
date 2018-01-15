3218da1f03a3940b793e46c5b436fc4dea0de05f
Brent Labasan <BrentLabasan@gmail.com>
2018-01-15 10:47:31 -0800

    modal contents

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ class App extends Component {
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
 
-    this.state = { 
+    this.state = {
       showModal: false
-     };
+    };
 
   }
 
@@ -32,13 +32,83 @@ class App extends Component {
     // const popover = (
     //   <Popover id="modal-popover" title="popover">
     //     very popover. such engagement
-		// 	</Popover>
+    // 	</Popover>
     // );
     // const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
 
-    const mc1 = <div>foo</div>;
-    const mc2 = <div>bar</div>;
-    const mc3 = <div>baz</div>;
+    const mc1 = <div>Instructions on how to request an ATM are located at the bottom of the page, below the map.
+      I plan to put videos of the process for different wallets in the future.
+    </div>;
+
+    const mc2 = <div>
+      Here are some ideas for locations where a Stellar ATM would be useful.
+      <ul>
+        <li>foreign countries that your relatives live in</li>
+        <li>cities with developing startup communities</li>
+        <li>tech incubators</li>
+        <li>colleges</li>
+        <li>stores with traditional ATMs</li>
+      </ul>
+
+      TODO: Elaborate on these possibilities with distinct locations. Implement copying of their coordinates to clipboard.
+    </div>;
+
+    const mc3 = <div>
+      WORK IN PROGRESS
+
+      <h3>What is the status (beta, live) of this project?</h3>
+      <p>This project is in beta testing.</p>
+
+      <h3>Do I have to donate a certain amount of XLM?</h3>
+      <p>Any donation amount helps!</p>
+
+      <h3>What happens to the funds that I donate?</h3>
+      <p>They get sent to a public key address that I created myself. View the address' activity at &nbsp;
+        <a href="https://stellarchain.io/address/GDZAZWBLGY3FVHRSYFRQ2IT6J5XTNWQHZMRFOYIRGTBX7AOSF767JOLT" target="_blank">
+          StellarChain.io</a>. The address balance is currently managed in a desktop software wallet, and I plan to move them to a Ledger Nano S when it
+          comes in the mail.
+        
+      </p>
+
+      <h3>How do we know that you're not just going to run off with the donated money? Are you a Stellar employee?</h3>
+      <p>Great question! My name is Brent Labasan, I'm a web app developer in Seattle, and I just started learning about 
+      Stellar in December 2017 after getting into cryptocurrencies through Bitcoin and IOTA. You can check out my &nbsp;
+      <a href="https://www.linkedin.com/in/brentlabasan/" target="_blank">LinkedIn</a> and &nbsp;
+      <a href="https://www.brentvlabasan.com/" target="_blank">portfolio</a>.
+      <br/><br/>
+      I don't have official ties to the Stellar Team or the Stellar Development Foundation. I'm just a developer
+      who wanted to start exploring the Stellar network and protocol and see what it's capable of. To further my learning,
+      I thought of various app ideas to make use of the network,
+      and a web app to encourage people to donate to set up ATMs in different locations was the idea that best fit
+      with Stellar's mission statement and goals.
+      <br/><br/>
+      I hope to work closely with the Stellar Team with the funds donated.
+
+      </p>
+
+      {/* <h3></h3>
+      <p></p>
+
+      <h3></h3>
+      <p></p> */}
+    </div>;
+
+    const mc4 = <div>
+      Request A Stellar ATM is a passion project by Brent Labasan.
+Current Goals For Project
+<ul>
+        <li>Encourage people to donate XLM to encourage ATMs in various locations, especially the underbanked.</li>
+        <li>Distribute the donated XLM to the Stellar Development Foundation to further adoption.</li>
+        {/* <li></li>
+  <li></li> */}
+
+      </ul>
+    </div>;
+
+    const mc5 = <div>
+      BrentLabasan@gmail.com
+
+</div>;
 
     return (
       <div className="App">
@@ -70,10 +140,10 @@ class App extends Component {
               </NavDropdown> */}
             </Nav>
             <Nav pullRight>
-              <NavItem eventKey={1} href="#">
+              <NavItem eventKey={1} href="#" onClick={() => this.handleShow(mc4)}>
                 ABOUT
 				</NavItem>
-              <NavItem eventKey={2} href="#">
+              <NavItem eventKey={2} href="#" onClick={() => this.handleShow(mc5)}>
                 CONTACT
 				</NavItem>
             </Nav>
